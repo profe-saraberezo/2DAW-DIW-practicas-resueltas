397149cb5dc9bbf08f4578b443aeb48e84306cca
profe-saraberezo <profe.saraberezo@gmail.com>
2024-11-26 13:23:01 +0100

    cartas

---
+++ UD 5. Animaciones/EJ11. Cartas memory/script.js
@@ -0,0 +1,30 @@
+var carta = null;
+
+function girarCarta(cartaGirada) {
+    if (!cartaGirada.classList.contains('girada')) {
+        cartaGirada.classList.add('girada');
+        setTimeout(compararCarta, 1000, cartaGirada);
+    }
+}
+
+function compararCarta(cartaDos) {
+
+    if(carta == null) {
+        carta = cartaDos;
+        console.log("Guardo carta");
+        return;
+    }
+    /* cartaDos.id */
+    /* cartaDos.attributes['name'].value */
+
+    if (cartaDos.id == carta.id) {
+        console.log("Son iguales");
+    }
+    else {
+        console.log("Son distintas");
+        carta.classList.remove('girada');
+        cartaDos.classList.remove('girada');
+    }
+    carta = null;
+
+}
